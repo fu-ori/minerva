900efda94e2afae9c6470b7a4e127462f8d80ab1
FU-ORI プロの職人 <yzbruno@gmail.com>
2025-03-18 17:03:06 -0300

    v0.6

--- single.php
+++ single.php
@@ -3,22 +3,25 @@
 get_header();
 $post_id = get_the_ID();
 
-$author_id = get_post_field('post_author', get_the_ID());
-$author_name = get_the_author_meta('display_name', $author_id);
+$author_id = get_post_field('post_author', $post_id);
+$author_name = esc_html(get_the_author_meta('display_name', $author_id));
+$author_url  = esc_url(get_author_posts_url($author_id));
+$post_date   = esc_html(get_the_date('d/m/Y'));
 
 ?>
 
-<section class="hero">
+<section class="hero" id="single">
     <div class="hero-body">
         <div class="container">
 
             <div class="box">
-                <h1 class="title is-2"><?php the_title(); ?></h1>
-                <h1 class="subtitle is-2"><?php the_title(); ?></h1>
-                <small class="tag">
-                    <?php echo "Autor: " . $author_name; ?>
-                </small>
+                <div class="has-text-right">
+                    <small class="tag">
+                        <?php echo $post_date . " - Publicado por: &nbsp;<a href='" . $author_url . "'>" . $author_name . "</a>"; ?>
+                    </small>
+                </div>
 
+                <h1 class="subtitle is-2"><?php the_title(); ?></h1>
                 <hr>
                 <p>
                     <?php the_content(); ?>
